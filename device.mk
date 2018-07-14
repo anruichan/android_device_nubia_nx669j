a9bfba609c3584c701737560f59bbf87a53b1748
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-07-14 20:29:40 +0200

    nx669j: Add a KeyHandler for GameSwitchkey events
    Change-Id: I22a7ee5b7eec5667b14f1a587a6d0138e77dc0f3

--- device.mk
+++ device.mk
@@ -227,6 +227,10 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     ueventd.qcom.rc
 
+# KeyHandler
+PRODUCT_PACKAGES += \
+    KeyHandler
+
 # Keymaster
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
