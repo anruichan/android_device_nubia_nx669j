f0032cd1de97c86186ee5e58f13a561027b00871
Tim Zimmermann <tim@linux4.de>
2022-08-23 16:57:23 +0200

    nx669j: FanControl: Include SettingsLib in static libraries
    * And remove now redundant dependencies
    
    Change-Id: Id5e790c6de0565592357d2fef11639d0b1533f14

--- FanControl/Android.mk
+++ FanControl/Android.mk
@@ -4,8 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_STATIC_ANDROID_LIBRARIES := \
-    androidx.core_core \
-    androidx.preference_preference
+    SettingsLib
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := FanControl
@@ -25,8 +24,6 @@ LOCAL_RESOURCE_DIR := $(package_resource_overlays) $(LOCAL_RESOURCE_DIR)
 
 LOCAL_PROGUARD_ENABLED := disabled
 LOCAL_DEX_PREOPT := false
-
-include frameworks/base/packages/SettingsLib/common.mk
 
 include $(BUILD_PACKAGE)
 
