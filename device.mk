326a10b156eaddb8304d2da8b7ff8324744e1563
Rohit R Sekhar <merothh@e.email>
2022-11-17 21:27:27 +0530

    nx669j: Build init.qti.display_boot scripts
    Change-Id: I8e77710f2490587b811fd6238491a92900c35d34

--- device.mk
+++ device.mk
@@ -126,6 +126,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.light-service.lineage \
+    init.qti.display_boot.rc \
+    init.qti.display_boot.sh \
     libmemutils \
     libqdMetaData.system \
     libsdmcore \
