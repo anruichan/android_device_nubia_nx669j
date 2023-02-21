df9ff9ed3b2ffa8b0ffc9ee7df2ec0cfbc5991b8
Albert Tang <tangalbert919@yahoo.com>
2023-02-21 11:00:11 -0600

    nx669j: Import task_profiles.json from NX669J_CNCommon_V8.21
    Change-Id: I922e83abeb47706303f2b83698ea07c09a65b069

--- device.mk
+++ device.mk
@@ -356,6 +356,10 @@ PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.systemhelper@1.0.vendor
 
+# Task profiles
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Telephony
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
