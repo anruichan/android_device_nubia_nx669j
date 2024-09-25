4d5ddb233863c58350ea58578f882b8f661251cd
Michael Bestas <mkbestas@lineageos.org>
2024-09-26 01:39:32 +0300

    nx669j: Switch to AIDL thermal HAL
    Change-Id: I6ae621194044ab66fad69b9ef997b3b347bf6120

--- device.mk
+++ device.mk
@@ -392,7 +392,7 @@ PRODUCT_BOOT_JARS += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti
+    android.hardware.thermal-service.qti
 
 # TrustedUI
 PRODUCT_PACKAGES += \
