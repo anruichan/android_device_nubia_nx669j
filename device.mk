3c3ce08156406065b65ba0181d725e8a916196f8
anruichan <anrui2032@gmail.com>
2024-12-24 20:18:20 +0800

    Revert "sake: Update to camera provider service to 2.5."
    This reverts commit 80a17bb1fc51908b20be96fea6b63c9884081d6e.
    
    Change-Id: I23f98ecf09b1425f53101c085a626d0ff87a3c4c

--- device.mk
+++ device.mk
@@ -106,7 +106,9 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     Snap \
-    android.hardware.camera.provider@2.5-service_64 \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64 \
+    vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
 # DRM
