3ee73600d9a5f4579ccf5fe372073cc326080cfe
anruichan <anrui2032@gmail.com>
2025-01-01 15:13:43 +0800

    nx669j: Build vendor variant android.hardware.secure_element@1.2
    Change-Id: Id8f9ead68cfe12a5aaba83c3acaebc5d281e2608

--- device.mk
+++ device.mk
@@ -320,7 +320,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
     android.hardware.radio.config@1.2.vendor \
-    android.hardware.radio.deprecated@1.0.vendor
+    android.hardware.radio.deprecated@1.0.vendor \
+    android.hardware.secure_element@1.2.vendor
 
 # Security
 BOOT_SECURITY_PATCH := 2023-10-01
