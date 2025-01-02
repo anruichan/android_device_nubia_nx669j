fb0d14f3760b89df7a7ed9285a60010b3ddc05d4
dianlujitao <dianlujitao@lineageos.org>
2025-01-02 11:34:02 +0800

    nx669j: move to aidl bootctrl hal
    Change-Id: I50fd09148097f0cda03c1a105340a3fd5f55ce8d

--- device.mk
+++ device.mk
@@ -96,9 +96,8 @@ PRODUCT_PACKAGES += \
 
 # Boot Control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl-qti \
-    android.hardware.boot@1.2-impl-qti.recovery \
-    android.hardware.boot@1.2-service
+    android.hardware.boot-service.qti \
+    android.hardware.boot-service.qti.recovery
 
 # Camera
 PRODUCT_COPY_FILES += \
