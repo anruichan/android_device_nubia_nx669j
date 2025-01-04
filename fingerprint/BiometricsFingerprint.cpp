2172b746a1b2c586b9758f76ac490d20cc18819f
anruichan <anrui2032@gmail.com>
2025-01-04 18:07:15 +0800

    Revert "sake: fingerprint: Add a cmd runner thread to not block on HIDL calls"
    This reverts commit efc64941d6a9718bfcca18efda4ebaa72d299012.
    
    Change-Id: I3aa11487ce359e4ea2cbf4219bfcbeab7b3754fc

--- fingerprint/BiometricsFingerprint.cpp
+++ fingerprint/BiometricsFingerprint.cpp
@@ -79,16 +79,6 @@ BiometricsFingerprint::BiometricsFingerprint() : mClientCallback(nullptr), mDevi
     this->mGoodixFingerprintDaemon = IGoodixFingerprintDaemon::getService();
 
     std::thread([this]() {
-        unsigned int cmd;
-
-        while (true) {
-            mCmdQueue.pop(cmd);
-            this->mGoodixFingerprintDaemon->sendCommand(cmd, {},
-                                                        [](int, const hidl_vec<signed char>&) {});
-        }
-    }).detach();
-
-    std::thread([this]() {
         int fd = open(FOD_UI_PATH, O_RDONLY);
         if (fd < 0) {
             LOG(ERROR) << "failed to open fd, err: " << fd;
@@ -109,7 +99,8 @@ BiometricsFingerprint::BiometricsFingerprint() : mClientCallback(nullptr), mDevi
             }
 
             if (readBool(fd)) {
-                mCmdQueue.push(CMD_LIGHT_AREA_STABLE);
+                this->mGoodixFingerprintDaemon->sendCommand(CMD_LIGHT_AREA_STABLE, {},
+                                                            [](int, const hidl_vec<signed char>&) {});
             }
         }
     }).detach();
@@ -135,13 +126,15 @@ Return<bool> BiometricsFingerprint::isUdfps(uint32_t) {
 }
 
 Return<void> BiometricsFingerprint::onFingerDown(uint32_t, uint32_t, float, float) {
-    mCmdQueue.push(CMD_FINGER_DOWN);
+    this->mGoodixFingerprintDaemon->sendCommand(CMD_FINGER_DOWN, {},
+                                                [](int, const hidl_vec<signed char>&) {});
 
     return Void();
 }
 
 Return<void> BiometricsFingerprint::onFingerUp() {
-    mCmdQueue.push(CMD_FINGER_UP);
+    this->mGoodixFingerprintDaemon->sendCommand(CMD_FINGER_UP, {},
+                                                [](int, const hidl_vec<signed char>&) {});
 
     return Void();
 }
