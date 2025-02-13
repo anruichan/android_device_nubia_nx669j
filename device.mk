9322ac97abfbf85e8e2e2344521a60f3bdb3ade2
anruichan <anrui2032@gmail.com>
2025-02-13 19:18:30 +0800

    nx669j: Build vendor variant libutilscallstack
    Change-Id: Ie3750545a7517e747e20e8548a3f8ff2c32b620a

--- device.mk
+++ device.mk
@@ -125,6 +125,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
 PRODUCT_PACKAGES += \
+    libutilscallstack.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
