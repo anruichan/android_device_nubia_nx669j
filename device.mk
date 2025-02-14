752fd942957324242fd283635497a29702f54989
anruichan <anrui2032@gmail.com>
2025-02-15 00:48:03 +0800

    Revert "sake: Add support for booting from EROFS dynamic images"
    This reverts commit 40f3cc7952f97f46368ce4d1bb9feec66804e9d2.
    
    Change-Id: I3c664853e45a4bd11c520fcbcabe8ffb9a737598

--- device.mk
+++ device.mk
@@ -10,18 +10,16 @@ $(call inherit-product, vendor/nubia/nx669j/nx669j-vendor.mk)
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
-PRODUCT_RO_FILE_SYSTEM ?= ext4
-
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=$(PRODUCT_RO_FILE_SYSTEM) \
+    FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_vendor=true \
     POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
-    FILESYSTEM_TYPE_vendor=$(PRODUCT_RO_FILE_SYSTEM) \
+    FILESYSTEM_TYPE_vendor=ext4 \
     POSTINSTALL_OPTIONAL_vendor=true
 
 PRODUCT_PACKAGES += \
