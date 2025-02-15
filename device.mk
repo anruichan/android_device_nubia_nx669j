322d381cab269bf79b8eaf55bf62ab9549f4ddf7
anruichan <anrui2032@gmail.com>
2025-02-15 23:58:13 +0800

    Revert "sake: Switch back to QTI AIDL vibrator HAL."
    This reverts commit c89b2e79f50a34efa09f3b04f3ab98ac937044ba.
    
    Change-Id: Ic32fa484a30dbfb307a993f48281015f2d06c6a3

--- device.mk
+++ device.mk
@@ -416,7 +416,7 @@ PRODUCT_COPY_FILES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+    vendor.qti.hardware.vibrator.service.nx669j
 
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
