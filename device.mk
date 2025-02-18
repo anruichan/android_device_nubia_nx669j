780eeeabb94c33e915c9876f2803c0d65794e468
anruichan <anrui2032@gmail.com>
2025-02-18 15:24:10 +0800

    nx669j: Build vendor variant libprocessgroup
    Change-Id: I9f0ca541995d2856edcd7c20464cb4d5d792e5a1

--- device.mk
+++ device.mk
@@ -78,6 +78,9 @@ PRODUCT_PACKAGES += \
     libtinycompress \
     libvolumelistener
 
+PRODUCT_PACKAGES += \
+    libprocessgroup.vendor
+
 # Authsecret
 PRODUCT_PACKAGES += \
     android.hardware.authsecret@1.0.vendor
@@ -185,7 +188,7 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss.measurement_corrections@1.1.vendor \
     android.hardware.gnss.visibility_control@1.0.vendor \
     android.hardware.gnss@2.1.vendor \
-    android.hardware.power@1.2.vendor
+    android.hardware.power@1.2.vendor \
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
