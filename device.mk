0d1a0205a00f8c77a1e6dc8585f476e31960f806
anruichan <anrui2032@gmail.com>
2025-02-18 15:26:30 +0800

    nx669j: Build vendor variant android.hardware.power@1.2
    Change-Id: Ia9a92481481c621fe39722add4db61743956c21f

--- device.mk
+++ device.mk
@@ -184,7 +184,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.gnss.measurement_corrections@1.1.vendor \
     android.hardware.gnss.visibility_control@1.0.vendor \
-    android.hardware.gnss@2.1.vendor
+    android.hardware.gnss@2.1.vendor \
+    android.hardware.power@1.2.vendor
 
 # GSI
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
